1d43e56cd944c9360144a44f449b9ece47fe4a40
vishnu <vishnupgithub@gmail.com>
2022-04-29 15:13:25 +0530

    notes and layout added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,13 +12,42 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true,
+      appBar: AppBar(
+        centerTitle: true,
         backgroundColor: Colors.white,
-        title: Text(
+        title: const Text(
           'Xylophone',
           style: TextStyle(color: Colors.purple, fontWeight: FontWeight.bold),
         ),
       ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            button(Colors.yellow, 1),
+            button(Colors.black, 2),
+            button(Colors.amberAccent, 3),
+            button(Colors. greenAccent, 4),
+            button(Colors.deepOrange, 5),
+            button(Colors.lime, 6),
+            button(Colors.deepPurpleAccent, 7),
+          ]),
+    );
+  }
+
+  void playMusic(int notesNumber) {
+    final player = AudioCache();
+    player.play('note$notesNumber.wav');
+  }
+
+  Widget button(Color color, int number) {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(primary: color),
+      onPressed: () {
+        playMusic(1);
+      },
+      icon: Icon(Icons.music_note),
+      label: Text(''),
     );
   }
 }
